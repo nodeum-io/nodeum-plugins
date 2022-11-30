6e1f22d918ebca7493cd679ba3dbea53f64f44d0
Thomas Chandelle <thomas.chandelle@mt-c.com>
2022-11-30 12:10:19 +0100

    Remove proto-related code

--- plugins/handshake.go
+++
@@ -1,9 +0,0 @@
-package plugins
-
-import "github.com/hashicorp/go-plugin"
-
-var Handshake = plugin.HandshakeConfig{
-	ProtocolVersion:  1,
-	MagicCookieKey:   "NODEUM_PLUGIN",
-	MagicCookieValue: "dc74089c-701c-4030-baed-add7c5db275f",
-}
