8f5e9d67f03a35a7fe999a703e19eb0bc1d95387
Thomas Chandelle <thomas.chandelle@mt-c.com>
2023-08-14 10:31:01 +0200

    Additional options for writer

--- storage/options.go
+++ storage/options.go
@@ -5,6 +5,9 @@ type WriterOptions struct {
 	Exclusive bool
 	// NodeInfo indicate metadata that also need to be applied.
 	NodeInfo *NodeInfo
+
+	// Opts contains additional options, defined by task
+	Opts map[string]interface{}
 }
 
 type WriterOption func(o *WriterOptions)
@@ -18,5 +21,11 @@ func WithExclusive(v bool) WriterOption {
 func WithNodeInfo(v *NodeInfo) WriterOption {
 	return func(o *WriterOptions) {
 		o.NodeInfo = v
+	}
+}
+
+func WithAdditionalOptions(v map[string]interface{}) WriterOption {
+	return func(o *WriterOptions) {
+		o.Opts = v
 	}
 }
